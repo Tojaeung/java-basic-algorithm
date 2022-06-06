3bb667d6b2ab14453cef7e80d02aae5ab0652b78
Tojaeung <kimwoon11@naver.com>
2022-06-07 00:34:09 +0900

    선택정렬 구현

--- src/basic/App.java
+++
@@ -1,7 +0,0 @@
-package basic;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
---
+++ src/basic/SelectionSort.java
@@ -0,0 +1,34 @@
+package basic;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) throws Exception {
+        // 다음의 무작위로 배열된 숫자들을 선택정렬한다.
+        int[] nums = { 1, 10, 5, 8, 7, 6, 4, 3, 2, 9 };
+
+        System.out.println("<정렬 전>");
+        System.out.println(Arrays.toString(nums));
+
+        for (int i = 0; i < nums.length; i++) {
+            // 루프를 돌떄 최솟값이 있는 index
+            int minIndex = i;
+
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[minIndex] > nums[j]) {
+                    // 최솟값이 있는 index를 찾았다.
+                    minIndex = j;
+                }
+            }
+
+            // 최소값이 있는 index와 i(시작 index )의 값을 바꿔준다.
+            int temp = nums[minIndex];
+            nums[minIndex] = nums[i];
+            nums[i] = temp;
+
+        }
+        System.out.println("<정렬 후>");
+        System.out.println(Arrays.toString(nums));
+    }
+
+}
